6086c63f7a6132a0ae73aae8c02d7e109348cf68
dawidzabrzenski <dawidzabrzenski00@gmail.com>
2023-06-06 13:40:55 +0200

    First commit Started first section of the course

---
+++ Learning_01/assignments.js
@@ -0,0 +1,26 @@
+// Lecture: Values and Variables
+
+// let country = "Poland";
+const country = "Poland";
+// let continent = "Europe";
+const continent = "Europe";
+let population = "37,75 millions";
+
+console.log(country, continent, population);
+
+// Lecture: Data Types
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// Lecture: let, const, var
+
+language = "Polish";
+console.log(language);
+
+// Lecture: Basic Operators
